4c3420033a334f2665b94a5747224287cc4d38a4
Nguyen Duy Thanh <nguyenduythanh25790@gmail.com>
2017-03-23 23:19:24 +0700

    abc

--- app/src/main/java/tu/pccc/pccc/SysConstraints.java
+++ app/src/main/java/tu/pccc/pccc/SysConstraints.java
@@ -10,8 +10,8 @@ public class SysConstraints {
     public static final String BASIC_AUTH_PASSWORD = "basicAuthPassword";
     public static final String SOCKET_URL = "http://103.48.83.139:8899";
     public static final String SERVICE_URL = "http://103.48.83.139:8898";
-//    public static final String SOCKET_URL = "http://192.168.1.121:8899";
-//    public static final String SERVICE_URL = "http://192.168.1.121:8898";
+//    public static final String SOCKET_URL = "http://192.168.0.112:8899";
+//    public static final String SERVICE_URL = "http://192.168.0.112:8898";
     public static final String KEY_MARKER_ID = "KEY_MARKER_ID";
     public static final String KEY_NAME = "KEY_NAME";
 }
